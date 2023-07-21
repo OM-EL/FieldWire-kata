47121e7699840f255ecf215ecc5f25ddece8942c
omarelhachimi <oelhachimi@solocal.com>
2023-07-21 18:31:17 +0200

    first commit

--- src/main/java/com/example/fieldwire/conf/AWSConfig.java
+++ src/main/java/com/example/fieldwire/conf/AWSConfig.java
@@ -1,6 +1,8 @@
 package com.example.fieldwire.conf;
 
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +13,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AWSConfig {
 
-
     @Bean
     public S3Client s3Client() {
-        return S3Client.builder().build();
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create("AKIASOO5UORSEISAGEXG", "QyUpPTxDj43qoUPKNLOlpWO2im4iVOeEknUXjEpt");
+
+        return S3Client.builder()
+                .region(Region.EU_WEST_1) // replace "YOUR_REGION" with your bucket's region
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
     }
 }
